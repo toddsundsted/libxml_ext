0f475d1ced5350a947e7699a35f73cb2d08d9142
Todd Sundsted <todd@sundsted.com>
2021-02-06 08:33:21 -0500

    Implement `XML::Node#add_sibling`.

--- spec/libxml_ext_spec.cr
+++ spec/libxml_ext_spec.cr
@@ -15,4 +15,14 @@ Spectator.describe "LibXML2 extensions" do
     expect(parent.xpath_node("//child")).to be_nil
     expect(parent.xpath_node("/parent/other")).to eq(other)
   end
+
+  it "adds a sibling after the node" do
+    parent.add_child(child).add_sibling(other, position: :after)
+    expect(parent.xpath_node("/parent/child/following-sibling::other")).to eq(other)
+  end
+
+  it "adds a sibling before the node" do
+    parent.add_child(child).add_sibling(other, position: :before)
+    expect(parent.xpath_node("/parent/child/preceding-sibling::other")).to eq(other)
+  end
 end
--- src/libxml_ext.cr
+++ src/libxml_ext.cr
@@ -3,6 +3,8 @@ require "xml"
 lib LibXML
   fun xmlAddChild(parent : Node*, child : Node*) : Node*
   fun xmlReplaceNode(node : Node*, other : Node*) : Node*
+  fun xmlAddNextSibling(node : Node*, other : Node*) : Node*
+  fun xmlAddPrevSibling(node : Node*, other : Node*) : Node*
 end
 
 struct XML::Node
@@ -18,6 +20,22 @@ struct XML::Node
   def replace_with(other : Node)
     LibXML.xmlUnlinkNode(other)
     LibXML.xmlReplaceNode(self, other)
+    other
+  end
+
+  # Adds a sibling before or after this node. By default, it adds the
+  # sibling after this node. Returns the sibling node.
+  def add_sibling(other : Node, position = :after)
+    case position
+    when :after
+      LibXML.xmlUnlinkNode(other)
+      LibXML.xmlAddNextSibling(self, other)
+    when :before
+      LibXML.xmlUnlinkNode(other)
+      LibXML.xmlAddPrevSibling(self, other)
+    else
+      raise NotImplementedError.new("position: #{position}")
+    end
     other
   end
 end
