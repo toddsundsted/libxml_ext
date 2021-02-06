cc5ada057033aad63bb6bde3710c1267c24643eb
Todd Sundsted <todd@sundsted.com>
2021-02-05 19:50:16 -0500

    Implement `XML::Node#add_child`.

---
+++ src/libxml_ext.cr
@@ -0,0 +1,13 @@
+lib LibXML
+  fun xmlAddChild(parent : Node*, child : Node*) : Node*
+end
+
+struct XML::Node
+  # Adds a child node to this node, after existing children.
+  # Returns the child node.
+  def add_child(child : Node)
+    LibXML.xmlUnlinkNode(child)
+    LibXML.xmlAddChild(self, child)
+    child
+  end
+end
