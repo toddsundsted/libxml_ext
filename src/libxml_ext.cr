70b87eeb4ea27894b42425b52280ea532c5965a5
Todd Sundsted <todd@sundsted.com>
2025-11-21 21:19:18 -0500

    Update Crystal Language version check.

--- src/libxml_ext.cr
+++ src/libxml_ext.cr
@@ -1,5 +1,9 @@
 require "xml"
 
+{% if compare_versions(Crystal::VERSION, "1.17.0") < 0 %}
+  {% raise "libxml_ext requires Crystal >= 1.17.0" %}
+{% end %}
+
 lib LibXML
   fun xmlNewText(content : UInt8*) : Node*
   fun xmlAddChild(parent : Node*, child : Node*) : Node*
@@ -10,56 +14,49 @@ lib LibXML
   fun xmlCopyDoc(node : Doc*, recursive : Int) : Doc*
 end
 
-{% begin %}
-  {% if compare_versions(Crystal::VERSION, "0.36.1") > 0 %}
-    class XML::Node
-  {% else %}
-    struct XML::Node
-  {% end %}
+class XML::Node
+  # Creates a new text node.
+  def initialize(text : String)
+    @node = LibXML.xmlNewText(text)
+  end
 
-    # Creates a new text node.
-    def initialize(text : String)
-      @node = LibXML.xmlNewText(text)
-    end
+  # Adds a child node to this node, after existing children, merging
+  # adjacent text nodes. Returns the child node.
+  def add_child(child : Node)
+    LibXML.xmlUnlinkNode(child)
+    LibXML.xmlAddChild(self, child)
+    child
+  end
 
-    # Adds a child node to this node, after existing children, merging
-    # adjacent text nodes. Returns the child node.
-    def add_child(child : Node)
-      LibXML.xmlUnlinkNode(child)
-      LibXML.xmlAddChild(self, child)
-      child
-    end
+  # Replaces this node with the other node. Returns the other node.
+  def replace_with(other : Node)
+    LibXML.xmlUnlinkNode(other)
+    LibXML.xmlReplaceNode(self, other)
+    other
+  end
 
-    # Replaces this node with the other node. Returns the other node.
-    def replace_with(other : Node)
+  # Adds a sibling before or after this node. By default, it adds the
+  # sibling after this node. Returns the sibling node.
+  def add_sibling(other : Node, position = :after)
+    case position
+    when :after
       LibXML.xmlUnlinkNode(other)
-      LibXML.xmlReplaceNode(self, other)
-      other
+      LibXML.xmlAddNextSibling(self, other)
+    when :before
+      LibXML.xmlUnlinkNode(other)
+      LibXML.xmlAddPrevSibling(self, other)
+    else
+      raise NotImplementedError.new("position: #{position}")
     end
+    other
+  end
 
-    # Adds a sibling before or after this node. By default, it adds the
-    # sibling after this node. Returns the sibling node.
-    def add_sibling(other : Node, position = :after)
-      case position
-      when :after
-        LibXML.xmlUnlinkNode(other)
-        LibXML.xmlAddNextSibling(self, other)
-      when :before
-        LibXML.xmlUnlinkNode(other)
-        LibXML.xmlAddPrevSibling(self, other)
-      else
-        raise NotImplementedError.new("position: #{position}")
-      end
-      other
-    end
-
-    # Performs a deep copy on this node.
-    def clone
-      self.class.new(LibXML.xmlCopyNode(self, 1)).tap do |clone|
-        self.class.new(LibXML.xmlCopyDoc(@node.value.doc, 0)).tap do |document|
-          document.add_child(clone)
-        end
+  # Performs a deep copy on this node.
+  def clone
+    self.class.new(LibXML.xmlCopyNode(self, 1)).tap do |clone|
+      self.class.new(LibXML.xmlCopyDoc(@node.value.doc, 0)).tap do |document|
+        document.add_child(clone)
       end
     end
   end
-{% end %}
+end
