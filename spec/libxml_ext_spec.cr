d8e88cac9a7dc7d09c9ee641c6e52a0521023412
Todd Sundsted <todd@sundsted.com>
2026-02-26 11:26:57 -0500

    Update library to address changes introduced in 1.17.x.
    See: https://github.com/toddsundsted/libxml_ext/issues/1
    See: https://github.com/crystal-lang/crystal/pull/15906

--- spec/libxml_ext_spec.cr
+++ spec/libxml_ext_spec.cr
@@ -1,39 +1,324 @@
 require "./spec_helper"
 
+class XML::Node
+  getter node
+end
+
+class XML::Document < XML::Node
+  getter cache
+  getter unlinked_nodes
+end
+
 Spectator.describe "LibXML2 extensions" do
-  let(parent) { XML.parse("<parent/>").first_element_child.not_nil! }
-  let(child) { XML.parse("<child/>").first_element_child.not_nil! }
-  let(other) { XML.parse("<other/>").first_element_child.not_nil! }
+  context "creating a text node" do
+    it "rejects text with null byte" do
+      expect { XML::Node.new("foo\0bar") }.to raise_error(ArgumentError, /null byte/)
+    end
 
-  it "adds a child" do
-    parent.add_child(child)
-    expect(parent.xpath_node("/parent/child")).to eq(child)
+    context "with valid text" do
+      subject { XML::Node.new("foo bar") }
+
+      it "is a text node" do
+        expect(subject.type).to eq(XML::Node::Type::TEXT_NODE)
+      end
+
+      it "has the text" do
+        expect(subject.text).to eq("foo bar")
+      end
+
+      it "sets its document" do
+        expect(subject.document).to be_a(XML::Document)
+      end
+
+      it "adds itself as a child of its document" do
+        expect(subject.document.children).to contain_exactly(subject)
+      end
+
+      it "adds itself to its document's cache" do
+        expect(subject.document.cache.keys).to have(subject.node)
+      end
+    end
   end
 
-  it "replaces a node" do
-    parent.add_child(child).replace_with(other)
-    expect(parent.xpath_node("//child")).to be_nil
-    expect(parent.xpath_node("/parent/other")).to eq(other)
+  context "adding a child node" do
+    let(parent) { XML.parse("<parent/>").first_element_child.not_nil! }
+    let(node) { XML.parse("<node/>").first_element_child.not_nil! }
+
+    def operation
+      parent.add_child(node)
+    end
+
+    it "adds the child to the parent" do
+      expect { operation }.to change { parent.xpath_node("/parent/node") }.from(nil).to(node)
+    end
+
+    it "changes the child's document" do
+      expect { operation }.to change { node.document }.from(node.document).to(parent.document)
+    end
+
+    let!(node_doc) { node.document }
+    let!(parent_doc) { parent.document }
+
+    it "removes the child from the child document's cache" do
+      expect { operation }.to change { node_doc.cache.size }.by(-1)
+    end
+
+    it "adds the child to the parent document's cache" do
+      expect { operation }.to change { parent_doc.cache.size }.by(1)
+    end
+
+    post_condition do
+      expect(node_doc.unlinked_nodes).not_to have(node.node)
+      expect(parent_doc.unlinked_nodes).not_to have(node.node)
+    end
+
+    context "when child was already added elsewhere" do
+      let(other) { XML.parse("<other/>").first_element_child.not_nil! }
+
+      before_each { parent.add_child(node) }
+
+      def operation
+        other.add_child(node)
+      end
+
+      it "removes the child from the original parent" do
+        expect { operation }.to change { parent.xpath_node("//node") }.from(node).to(nil)
+      end
+
+      it "adds the child to the other parent" do
+        expect { operation }.to change { other.xpath_node("/other/node") }.from(nil).to(node)
+      end
+
+      it "changes the child's document" do
+        expect { operation }.to change { node.document }.from(parent.document).to(other.document)
+      end
+
+      let!(other_doc) { other.document }
+
+      it "removes the child from the parent document's cache" do
+        expect { operation }.to change { parent_doc.cache.size }.by(-1)
+      end
+
+      it "adds the child to the other document's cache" do
+        expect { operation }.to change { other_doc.cache.size }.by(1)
+      end
+
+      post_condition do
+        expect(other_doc.unlinked_nodes).not_to have(node.node)
+      end
+    end
+
+    context "given a subtree" do
+      let(other) { XML.parse("<other/>").first_element_child.not_nil! }
+
+      before_each { parent.add_child(node) }
+
+      def operation
+        other.add_child(parent)
+      end
+
+      it "adds the child to the parent" do
+        expect { operation }.to change { other.xpath_node("/other/parent/node") }.from(nil).to(node)
+      end
+
+      it "changes the child's document" do
+        expect { operation }.to change { node.document }.from(parent.document).to(other.document)
+      end
+
+      let!(other_doc) { other.document }
+
+      it "removes the child from the parent document's cache" do
+        expect { operation }.to change { parent_doc.cache.size }.by(-2)
+      end
+
+      it "adds the child to the other document's cache" do
+        expect { operation }.to change { other_doc.cache.size }.by(2)
+      end
+
+      post_condition do
+        expect(other_doc.unlinked_nodes).not_to have(node.node)
+      end
+    end
+
+    context "given a text node" do
+      let(text) { XML::Node.new("text") }
+
+      def operation
+        parent.add_child(text)
+      end
+
+      it "adds the text node to the parent" do
+        expect { operation }.to change { parent.xpath_node("/parent/text()") }.from(nil).to(text)
+      end
+
+      it "changes the text node's document" do
+        expect { operation }.to change { text.document }.from(text.document).to(parent.document)
+      end
+
+      let!(text_doc) { text.document }
+      let!(parent_doc) { parent.document }
+
+      it "removes the text node from the text node document's cache" do
+        expect { operation }.to change { text_doc.cache.has_key?(text.node) }.from(true).to(false)
+      end
+
+      it "adds the text node to the parent document's cache" do
+        expect { operation }.to change { parent_doc.cache.has_key?(text.node) }.from(false).to(true)
+      end
+
+      post_condition do
+        expect(text_doc.unlinked_nodes).not_to have(text.node)
+        expect(parent_doc.unlinked_nodes).not_to have(text.node)
+      end
+    end
   end
 
-  it "adds a sibling after the node" do
-    parent.add_child(child).add_sibling(other, position: :after)
-    expect(parent.xpath_node("/parent/child/following-sibling::other")).to eq(other)
+  context "replacing a node" do
+    let(parent) { XML.parse("<parent><node/></parent>").first_element_child.not_nil! }
+    let(other) { XML.parse("<other/>").first_element_child.not_nil! }
+    let(node) { parent.xpath_node("/parent/node").not_nil! }
+
+    def operation
+      node.replace_with(other)
+    end
+
+    it "removes node from the parent" do
+      expect { operation }.to change { parent.xpath_node("/parent/node") }.from(node).to(nil)
+    end
+
+    it "adds other to the parent" do
+      expect { operation }.to change { parent.xpath_node("/parent/other") }.from(nil).to(other)
+    end
+
+    it "does not change the node's document" do
+      expect { operation }.not_to change { node.document }
+    end
+
+    it "changes the other's document" do
+      expect { operation }.to change { other.document }.from(other.document).to(parent.document)
+    end
+
+    let!(node_doc) { node.document }
+    let!(other_doc) { other.document }
+    let!(parent_doc) { parent.document }
+
+    it "does not remove node from the document's cache" do
+      expect { operation }.not_to change { node_doc.cache.has_key?(node.@node) }
+    end
+
+    it "removes other from other document's cache" do
+      expect { operation }.to change { other_doc.cache.size }.by(-1)
+    end
+
+    it "adds other to the parent document's cache" do
+      expect { operation }.to change { parent_doc.cache.size }.by(1)
+    end
+
+    post_condition do
+      expect(node_doc.unlinked_nodes).to have(node.node)
+      expect(other_doc.unlinked_nodes).not_to have(other.node)
+    end
+
+    context "given a text node" do
+      let(text) { XML::Node.new("text") }
+
+      def operation
+        node.replace_with(text)
+      end
+
+      it "replaces node with text" do
+        expect { operation }.to change { parent.xpath_node("/parent/child::node()") }.from(node).to(text)
+      end
+    end
   end
 
-  it "adds a sibling before the node" do
-    parent.add_child(child).add_sibling(other, position: :before)
-    expect(parent.xpath_node("/parent/child/preceding-sibling::other")).to eq(other)
+  context "adding a sibling after a node" do
+    let(parent) { XML.parse("<parent><node/></parent>").first_element_child.not_nil! }
+    let(other) { XML.parse("<other/>").first_element_child.not_nil! }
+    let(node) { parent.xpath_node("/parent/node").not_nil! }
+
+    def operation
+      node.add_sibling(other, position: :after)
+    end
+
+    it "adds other after the node" do
+      expect { operation }.to change { parent.xpath_node("/parent/node/following-sibling::other") }.from(nil).to(other)
+    end
+
+    it "changes the other's document" do
+      expect { operation }.to change { other.document }.from(other.document).to(parent.document)
+    end
+
+    let!(other_doc) { other.document }
+    let!(parent_doc) { parent.document }
+
+    it "removes other from other document's cache" do
+      expect { operation }.to change { other_doc.cache.size }.by(-1)
+    end
+
+    it "adds other to the parent document's cache" do
+      expect { operation }.to change { parent_doc.cache.has_key?(other.@node) }.from(false).to(true)
+    end
+
+    post_condition do
+      expect(other_doc.unlinked_nodes).not_to have(other.node)
+    end
+
+    context "given a text node" do
+      let(text) { XML::Node.new("text") }
+
+      def operation
+        node.add_sibling(text, position: :after)
+      end
+
+      it "adds text after the node" do
+        expect { operation }.to change { parent.xpath_node("/parent/node/following-sibling::text()") }.from(nil).to(text)
+      end
+    end
   end
 
-  it "creates a text node" do
-    parent.add_child(XML::Node.new("foobar"))
-    expect(parent.xpath_node("/parent/text()").try(&.text)).to eq("foobar")
-  end
+  context "adding a sibling before a node" do
+    let(parent) { XML.parse("<parent><node/></parent>").first_element_child.not_nil! }
+    let(other) { XML.parse("<other/>").first_element_child.not_nil! }
+    let(node) { parent.xpath_node("/parent/node").not_nil! }
 
-  it "clones the node" do
-    parent.add_child(child).add_child(XML::Node.new("foobar"))
-    expect(child.clone.xpath_node("/child/text()").try(&.text)).to eq("foobar")
-    expect(child.clone).not_to eq(child)
+    def operation
+      node.add_sibling(other, position: :before)
+    end
+
+    it "adds other before the node" do
+      expect { operation }.to change { parent.xpath_node("/parent/node/preceding-sibling::other") }.from(nil).to(other)
+    end
+
+    it "changes the other's document" do
+      expect { operation }.to change { other.document }.from(other.document).to(parent.document)
+    end
+
+    let!(other_doc) { other.document }
+    let!(parent_doc) { parent.document }
+
+    it "removes other from other document's cache" do
+      expect { operation }.to change { other_doc.cache.size }.by(-1)
+    end
+
+    it "adds other to the parent document's cache" do
+      expect { operation }.to change { parent_doc.cache.has_key?(other.@node) }.from(false).to(true)
+    end
+
+    post_condition do
+      expect(other_doc.unlinked_nodes).not_to have(other.node)
+    end
+
+    context "given a text node" do
+      let(text) { XML::Node.new("text") }
+
+      def operation
+        node.add_sibling(text, position: :before)
+      end
+
+      it "adds text before the node" do
+        expect { operation }.to change { parent.xpath_node("/parent/node/preceding-sibling::text()") }.from(nil).to(text)
+      end
+    end
   end
 end
--- src/libxml_ext.cr
+++ src/libxml_ext.cr
@@ -1,65 +1,213 @@
 require "xml"
 
+{% if compare_versions(Crystal::VERSION, "1.19.1") < 0 %}
+  {% raise "libxml_ext requires Crystal >= 1.19.1" %}
+{% end %}
+
 lib LibXML
   fun xmlNewText(content : UInt8*) : Node*
   fun xmlAddChild(parent : Node*, child : Node*) : Node*
   fun xmlReplaceNode(node : Node*, other : Node*) : Node*
   fun xmlAddNextSibling(node : Node*, other : Node*) : Node*
   fun xmlAddPrevSibling(node : Node*, other : Node*) : Node*
-  fun xmlCopyNode(node : Node*, extended : Int) : Node*
-  fun xmlCopyDoc(node : Doc*, recursive : Int) : Doc*
+  fun xmlNewDoc(version : UInt8*) : Doc*
+  fun xmlDOMWrapAdoptNode(
+    ctxt : Void*,
+    sourceDoc : Doc*,
+    node : Node*,
+    destDoc : Doc*,
+    destParent : Node*,
+    options : Int32
+  ) : Int32
 end
 
-{% begin %}
-  {% if compare_versions(Crystal::VERSION, "0.36.1") > 0 %}
-    class XML::Node
-  {% else %}
-    struct XML::Node
-  {% end %}
+class XML::Node
+  # Returns a new text node.
+  #
+  def self.new(text : String)
+    raise ArgumentError.new("cannot include null byte") if text.includes?('\0')
+    doc = LibXML.xmlNewDoc(nil)
+    document = Document.new(doc)
+    text = LibXML.xmlNewText(text)
+    node = new(text, document)
+    LibXML.xmlAddChild(document, node)
+    node
+  end
 
-    # Creates a new text node.
-    def initialize(text : String)
-      @node = LibXML.xmlNewText(text)
-    end
-
-    # Adds a child node to this node, after existing children, merging
-    # adjacent text nodes. Returns the child node.
-    def add_child(child : Node)
-      LibXML.xmlUnlinkNode(child)
-      LibXML.xmlAddChild(self, child)
-      child
-    end
-
-    # Replaces this node with the other node. Returns the other node.
-    def replace_with(other : Node)
-      LibXML.xmlUnlinkNode(other)
-      LibXML.xmlReplaceNode(self, other)
-      other
-    end
-
-    # Adds a sibling before or after this node. By default, it adds the
-    # sibling after this node. Returns the sibling node.
-    def add_sibling(other : Node, position = :after)
-      case position
-      when :after
-        LibXML.xmlUnlinkNode(other)
-        LibXML.xmlAddNextSibling(self, other)
-      when :before
-        LibXML.xmlUnlinkNode(other)
-        LibXML.xmlAddPrevSibling(self, other)
-      else
-        raise NotImplementedError.new("position: #{position}")
-      end
-      other
-    end
-
-    # Performs a deep copy on this node.
-    def clone
-      self.class.new(LibXML.xmlCopyNode(self, 1)).tap do |clone|
-        self.class.new(LibXML.xmlCopyDoc(@node.value.doc, 0)).tap do |document|
-          document.add_child(clone)
-        end
+  # Adopts a node when crossing document boundaries.
+  #
+  private def adopt_node(node : Node, from : Document, to : Document)
+    if from.@node != to.@node
+      result = LibXML.xmlDOMWrapAdoptNode(nil, from.@node.as(LibXML::Doc*), node, to.@node.as(LibXML::Doc*), nil, 0)
+      if result != 0
+        raise XML::Error.new("xmlDOMWrapAdoptNode failed with code #{result}", 0)
       end
     end
   end
-{% end %}
+
+  # Recursively moves nodes.
+  #
+  # Enforces the following rules (from the library documentation):
+  #
+  #   ...when a libxml node is moved to another document, then the
+  #   @document reference of its XML::Node and any instantiated
+  #   descendant must be updated to point to the new document Node.
+  #
+  #   ...the libxml node, along with any descendant shall be removed
+  #   from the unlinked nodes list when relinked into a tree, be it
+  #   the same document or another.
+  #
+  #   ...when a XML::Node is moved to another document, the XML::Node
+  #   and any instantiated descendant XML::Node shall be cleaned from
+  #   the original document's cache, and must be added to the new
+  #   document's cache.
+  #
+  private def move_nodes(node : Node, from : Document, to : Document)
+    move_nodes(node.@node, from, to)
+  end
+
+  # :ditto:
+  private def move_nodes(node_p : Pointer(LibXML::Node), from : Document, to : Document)
+    if (ref = from.cache.delete(node_p))
+      to.cache[node_p] = ref
+      if (node = ref.value)
+        node.document = to
+      end
+    end
+    from.unlinked_nodes.delete(node_p)
+    node_p = node_p.value.children
+    while node_p
+      move_nodes(node_p, from, to)
+      node_p = node_p.value.next
+    end
+  end
+
+  protected setter document
+
+  # ## Text Node Implementation Note ##
+  #
+  # libxml2 has inconsistent text node merging behavior. Generally,
+  # when libxml2 functions (e.g. `xmlAddChild`, `xmlAddNextSibling`,
+  # `xmlAddPrevSibling`) add a text node adjacent to another text
+  # node, libxml2:
+  #
+  # 1. Merges the text content into the adjacent node
+  # 2. Frees the "added" node's memory
+  # 3. Returns a pointer to the merged node
+  #
+  # ### Important Exceptions ###
+  #
+  # - `element.add_sibling(text_node)` and `element.add_child(text_node)`:
+  #   When the **reference node** (the node calling the method) is an
+  #   element, adding a text node does not trigger merging even if an
+  #   **adjacent node** is a text node. The **reference node** must be
+  #   a text node to trigger merging.
+  #
+  # - `xmlReplaceNode(old, text_node)`: Never merges, even when replacing
+  #   an element between two adjacent text nodes.
+  #
+  # This behavior is **context-dependent** and **poorly documented**.
+  # The same function can either merge or not merge depending on the
+  # function called and type of the adjacent node. This makes it difficult
+  # to write safe library code without defensive measures.
+  #
+  # ### Placeholder Elements ###
+  #
+  # To normalize this, we use temporary placeholder elements when
+  # adding text nodes:
+  #
+  # 1. Create a temporary placeholder element (`<__libxml_ext_placeholder__/>`)
+  # 2. Add the placeholder using libxml2 functions
+  # 3. Replace the placeholder with the text node using `xmlReplaceNode()`
+  #
+  # Neither step 2 nor step 3 trigger text node merging.
+  #
+  # ### Methods Using This Technique
+  #
+  # - `add_child(text_node)`
+  # - `add_sibling(text_node)`
+
+  # Creates a placeholder element.
+  #
+  private def create_placeholder_element : Node
+    placeholder_doc = XML.parse("<__libxml_ext_placeholder__/>")
+    placeholder_doc.first_element_child.not_nil!
+  end
+
+  # Adds a child node to this node after any existing children.
+  #
+  # Returns the child node.
+  #
+  def add_child(child : Node)
+    if child.text?
+      placeholder = create_placeholder_element
+      add_child(placeholder)
+      placeholder.replace_with(child)
+    else
+      from_doc = child.document
+      to_doc = self.document
+      child.unlink
+      adopt_node(child, from_doc, to_doc)
+      if LibXML.xmlAddChild(self, child)
+        move_nodes(child, from_doc, to_doc)
+      else
+        raise XML::Error.new("xmlAddChild failed", 0)
+      end
+    end
+    child
+  end
+
+  # Replaces this node with the other node.
+  #
+  # Returns the other node.
+  #
+  def replace_with(other : Node)
+    from_doc = other.document
+    to_doc = self.document
+    other.unlink
+    adopt_node(other, from_doc, to_doc)
+    if LibXML.xmlReplaceNode(self, other)
+      self.document.unlinked_nodes.add(self.@node)
+      move_nodes(other, from_doc, to_doc)
+    else
+      raise XML::Error.new("xmlReplaceNode failed", 0)
+    end
+    other
+  end
+
+  enum Position
+    After
+    Before
+  end
+
+  # Adds a sibling before or after this node.
+  #
+  # By default, it adds the sibling after this node.
+  #
+  # Returns the sibling node.
+  #
+  def add_sibling(sibling : Node, position : Position = Position::After)
+    if sibling.text?
+      placeholder = create_placeholder_element
+      add_sibling(placeholder, position)
+      placeholder.replace_with(sibling)
+    else
+      from_doc = sibling.document
+      to_doc = self.document
+      sibling.unlink
+      adopt_node(sibling, from_doc, to_doc)
+      case position
+      in Position::After
+        unless LibXML.xmlAddNextSibling(self, sibling)
+          raise XML::Error.new("xmlAddNextSibling failed", 0)
+        end
+      in Position::Before
+        unless LibXML.xmlAddPrevSibling(self, sibling)
+          raise XML::Error.new("xmlAddPrevSibling failed", 0)
+        end
+      end
+      move_nodes(sibling, from_doc, to_doc)
+    end
+    sibling
+  end
+end
